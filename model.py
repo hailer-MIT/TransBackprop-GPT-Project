fd8401a1bba88b7b4ba5e91999d96efaa68df06b
unknown <hailomasegede@gmail.com>
2025-10-09 11:46:51 +0300

    commit 6

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from .config import GPTConfig
+from TransBackprop_GPT_Project.config import GPTConfig
 import math
 
 class CausalSelfAttention(nn.Module):
